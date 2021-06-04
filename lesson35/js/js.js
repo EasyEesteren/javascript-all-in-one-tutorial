00d0f3b092a3d5dcb2a8e28511c631c1e81153ad
Jamie van Eesteren <JAMIE_VANEESTEREN@CRGL-THIRDPARTY.COM>
2021-06-04 09:27:13 +0200

    up to lesson 35

---
+++ lesson35/js/js.js
@@ -0,0 +1,3 @@
+// Build pyramid
+
+var div = getElementById
--- lesson4-35/js/lesson.js
+++ lesson4-35/js/lesson.js
@@ -51,14 +51,66 @@ console.log(lol); */
 } */
 
 // Instance methods
-
-{
+/* {
     let x = 5.2222
     console.log(x.toFixed(2));
-}
+} */
 
 // Math object
-{
+/* {
     console.log(Math.round(0.5));
     console.log(Math.trunc(0.5));
+} */
+
+// This
+/* {
+    let position = {
+        x: 10,
+        y: 5,
+        print: function(){
+            console.log(this.x);
+            console.log(`X: ${this.x}, Y: ${this.y}`);
+        },
+    }
+
+    position.print()
+} */
+
+// Switch statements
+/* {
+    let name = prompt("What is your name?");
+
+    if (name=="HOT") alert("sweet diggly");
+
+    switch(name){
+        case "Caleb":
+            console.log("Welcome!");
+            break;
+        case "Jamie":
+            console.log("GET OUT!");
+            break;
+        default:
+            console.log("I guess you may enter....");
+            break;
+    }
+} */
+
+// Do while, useful if you want the code with the do to be executed at least once
+// Note varaibles defined in the do are not available in to the while statement
+/* let i = 1000;
+do{
+    console.log(i);
+    i++;
+} while(i < 10); */
+
+/* let password;
+do{
+    password = prompt("Guess the corret name");
+} while(password.toLowerCase() !== "jamie");
+alert(`Well done ${password} was the correct name!`) */
+
+
+// For loop
+for(let i = 0; i < 10; i++){
+    console.log(i);
 }
